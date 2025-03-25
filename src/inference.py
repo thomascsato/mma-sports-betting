7771a3ee192eadb1f4a6bd601d11fa2581bcfd00
thomascsato <thomascsato3@gmail.com>
2025-03-24 23:13:16 -0700

    Improve workflow

--- src/inference.py
+++ src/inference.py
@@ -1,44 +1,38 @@
 import joblib
 import pandas as pd
-"""
-print("Reading in models")
+import openpyxl
+
+"""print("Loading in models")
 post_fight_model = joblib.load('models\\post_fight_model.joblib')
 win_pred_model = joblib.load('models\\win_pred_model.joblib')
-print("Models read in successfully.")
+print("Models loaded successfully.")
 """
 # Column names
-pre_features_numeric = ["r_wins_agg",
-    "r_losses_agg", "r_height_agg", "r_weight_agg", "r_reach_agg", "r_age_agg",
-    "r_SLpM_agg", "r_sig_str_acc_agg", "r_SApM_agg", "r_str_def_agg",
-    "r_td_avg_agg", "r_td_acc_agg", "r_td_def_agg", "r_sub_avg_agg", "b_wins_agg",
-    "b_losses_agg", "b_height_agg", "b_weight_agg", "b_reach_agg", "b_age_agg",
-    "b_SLpM_agg", "b_sig_str_acc_agg", "b_SApM_agg", "b_str_def_agg",
-    "b_td_avg_agg", "b_td_acc_agg", "b_td_def_agg", "b_sub_avg_agg"]
+pre_features_numeric = [
+    "r_wins_agg", "r_losses_agg", "r_height_agg", "r_weight_agg", "r_reach_agg", 
+    "r_age_agg", "r_SLpM_agg", "r_sig_str_acc_agg", "r_SApM_agg", "r_str_def_agg",
+    "r_td_avg_agg", "r_td_acc_agg", "r_td_def_agg", "r_sub_avg_agg", 
+    "b_wins_agg", "b_losses_agg", "b_height_agg", "b_weight_agg", "b_reach_agg",
+    "b_age_agg", "b_SLpM_agg", "b_sig_str_acc_agg", "b_SApM_agg", "b_str_def_agg",
+    "b_td_avg_agg", "b_td_acc_agg", "b_td_def_agg", "b_sub_avg_agg"
+]
 pre_features_categorical = ["weight_class", "is_title_bout", "gender", "r_stance_agg", "b_stance_agg"]
 
-post_features_numeric = ["finish_round", "total_rounds", "time_sec", 
-        "r_kd", "r_sig_str", "r_sig_str_att", "r_sig_str_acc", "r_str",
-        "r_str_att", "r_str_acc", "r_td", "r_td_att", "r_td_acc",
-        "r_sub_att", "r_rev", "r_ctrl_sec", "b_kd", "b_sig_str",
-        "b_sig_str_att", "b_sig_str_acc", "b_str", "b_str_att", "b_str_acc",
-        "b_td", "b_td_att", "b_td_acc", "b_sub_att", "b_rev", "b_ctrl_sec"]
+post_features_numeric = [
+    "finish_round", "total_rounds", "time_sec", 
+    "r_kd", "r_sig_str", "r_sig_str_att", "r_sig_str_acc", "r_str",
+    "r_str_att", "r_str_acc", "r_td", "r_td_att", "r_td_acc",
+    "r_sub_att", "r_rev", "r_ctrl_sec", 
+    "b_kd", "b_sig_str", "b_sig_str_att", "b_sig_str_acc", "b_str",
+    "b_str_att", "b_str_acc", "b_td", "b_td_att", "b_td_acc",
+    "b_sub_att", "b_rev", "b_ctrl_sec"
+]
 
-# Read in data for automation
-fighters = pd.read_csv("data\\fighter_stats.csv")
-
-# Read in user input
-fighter_1 = input("Fighter 1: ")
-fighter_2 = input("Fighter 2: ")
-weight = input("Weight (Fl, Bw, Ft, Lw, Ww, Mw, Lh, Hw, Ws, Wf, Wb): ")
-is_title = [int(input("Title Fight? (1/0): "))]
-gender = [input("Gender (Male/Female): ")]
-
-fighter_1_stats = fighters[fighters["name"] == fighter_1]
-fighter_2_stats = fighters[fighters["name"] == fighter_2]
-
-col_ordering = ["wins", "losses", "height", "weight", "reach", "age", 
-                "SLpM", "sig_str_acc", "SApM", "str_def", 
-                "td_avg", "td_acc", "td_def", "sub_avg"]
+col_ordering = [
+    "wins", "losses", "height", "weight", "reach", "age", 
+    "SLpM", "sig_str_acc", "SApM", "str_def", 
+    "td_avg", "td_acc", "td_def", "sub_avg"
+]
 
 weight_classes = {
     "Fl" : "Flyweight",
@@ -54,30 +48,38 @@ weight_classes = {
     "Wb" : "Women's Bantamweight"
 }
 
-weight = [weight_classes[weight]]
+# Read in data for automation
+fighters = pd.read_csv("data\\fighter_stats.csv")
 
+# Read in spreadsheet to log predictions
+predictions = pd.read_excel("Predictions.xlsx")
 
+for row in predictions.itertuples(index=False):
 
-print(weight)
+    fighter_1 = row.Fighter_1
+    fighter_2 = row.Fighter_2
+    weight = row.Weight
+    is_title = [int(row.Title)]
+    gender = [row.Gender]
 
-fighters_concat = fighter_1_stats[col_ordering].iloc[0].tolist() + fighter_2_stats[col_ordering].iloc[0].tolist() + weight + is_title + gender + [fighter_1_stats["stance"].iloc[0]] + [fighter_2_stats["stance"].iloc[0]]
+    fighter_1_stats = fighters[fighters["name"] == fighter_1]
+    fighter_2_stats = fighters[fighters["name"] == fighter_2]
 
-print(fighters_concat)
+    weight = [weight_classes[weight]]
 
-"""# Train first model to predict 
-jon_jones_vs_stipe_miocic = pd.DataFrame([
-    [27, 1, 193.04, 112.49, 213.36, 37, 4.29, 0.57, 2.22, 0.64, 1.93, 0.45, 0.95, 0.5,
-    20, 4, 193.04, 108.86, 203.2, 42, 4.82, 0.53, 3.82, 0.54, 1.86, 0.34, 0.68, 0,
-    "UFC Heavyweight Title", 1, "Men", "Orthodox", "Orthodox"]
-], columns=pre_features_numeric + pre_features_categorical)
+    # List formatted
+    fighters_concat = fighter_1_stats[col_ordering].iloc[0].tolist() + fighter_2_stats[col_ordering].iloc[0].tolist() + weight + is_title + gender + [fighter_1_stats["stance"].iloc[0]] + [fighter_2_stats["stance"].iloc[0]]
 
-print("Calculating post fight stats predictions")
-post_fight_predictions = post_fight_model.predict(jon_jones_vs_stipe_miocic)
-print(post_fight_predictions)
+    # DataFrame formatted for input into model
+    fight = pd.DataFrame(fighters_concat, columns = pre_features_numeric + pre_features_categorical)
 
-post_fight_predictions = pd.DataFrame(post_fight_predictions, columns=post_features_numeric)
-combined_input = pd.concat([jon_jones_vs_stipe_miocic, post_fight_predictions], axis=1)
+    print("Calculating post fight stats predictions")
+    post_fight_predictions = post_fight_model.predict(fight)
+    print(post_fight_predictions)
 
-print("Calculating win probabilities")
-win_probabilities = win_pred_model.predict_proba(combined_input)
-print(f"Fighter 1 Win Probability: {win_probabilities[0][0]}\nFighter 2 Win Probability: {win_probabilities[0][1]}")"""
+    post_fight_predictions = pd.DataFrame(post_fight_predictions, columns=post_features_numeric)
+    combined_input = pd.concat([fight, post_fight_predictions], axis=1)
+
+    print("Calculating win probabilities")
+    win_probabilities = win_pred_model.predict_proba(combined_input)
+    print(f"{fighter_1} Win Probability: {win_probabilities[0][0]}\n{fighter_2} Win Probability: {win_probabilities[0][1]}")
